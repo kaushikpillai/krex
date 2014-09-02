577885072dadfcfebdde6c5c379d14b93bf3efcf
KaushiK Pillai <koushiktk09@gmail.com>
2014-09-02 11:58:42 +0530

    form-required fields

--- test.php
+++ test.php
@@ -10,6 +10,23 @@
 #echo $_SERVER['HTTP_USER_AGENT'];
 #echo "<br>";
 #echo $_SERVER['SCRIPT_NAME'];
+
+
+#$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+#sort($age);print_r($age);echo "<br>";
+#$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+#rsort($age);print_r($age);echo "<br>";
+#$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+#asort($age);print_r($age);echo "<br>";
+#$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+#ksort($age);print_r($age);echo "<br>";
+#$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+#arsort($age);print_r($age);echo "<br>";
+#$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+#krsort($age);print_r($age);echo "<br>";
+
+
+
 ?>
 
 <!-- <html>
@@ -77,17 +94,67 @@ print_r($_REQUEST);
  
  
  <html>
+ <style>
+.error {color: #FF0000;}
+</style>
  <body>
+ <?php
+  // define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   if (empty($_POST["name"])) {
+     $nameErr = "Name is required";
+   } else {
+     $name = test_input($_POST["name"]);
+   }
+   
+   if (empty($_POST["email"])) {
+     $emailErr = "Email is required";
+   } else {
+     $email = test_input($_POST["email"]);
+   }
+     
+   if (empty($_POST["website"])) {
+     $website = "";
+   } else {
+     $website = test_input($_POST["website"]);
+   }
+
+   if (empty($_POST["comment"])) {
+     $comment = "";
+   } else {
+     $comment = test_input($_POST["comment"]);
+   }
+
+   if (empty($_POST["gender"])) {
+     $genderErr = "Gender is required";
+   } else {
+     $gender = test_input($_POST["gender"]);
+   }
+}
+ ?>
  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  Name: <input type="text" name="name"><br>
- E-mail: <input type="text" name="email"><br>
-Website: <input type="text" name="website"><br>
+  Name: <input type="text" name="name">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br>
+  
+ E-mail: <input type="text" name="email">
+ <span class="error">* <?php echo $emailErr;?></span>
+ <br>
+Website: <input type="text" name="website">
+<span class="error"><?php echo $websiteErr;?></span>
+   <br><br>
 Comment: <textarea name="comment" rows="5" cols="40"></textarea><br>
 Gender:
 <input type="radio" name="gender" value="female">Female</input>
 <input type="radio" name="gender" value="male">Male</input>
+<span class="error">* <?php echo $genderErr;?></span>
+   <br>
 <input type="submit">
 </form>
+
 <?php
 // define variables and set to empty values
 $name = $email = $gender = $comment = $website = "";
@@ -108,6 +175,8 @@ function test_input($data) {
   $data = htmlspecialchars($data);
   return $data;
  }
+ 
+
 ?>
  </body>
  </html>
